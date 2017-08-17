736c2f79f664b533df6da17d68cc2e709118912c
Maria Karanasou <karanasou@gmail.com>
2017-08-17 18:15:11 +0300

    Update and rename TextTagger.py to text_tagger.py

--- TweetUtils/feature_extraction/text_tagger.py
+++ TweetUtils/feature_extraction/text_tagger.py
@@ -2,7 +2,7 @@ __author__ = 'maria'
 
 import re
 import traceback
-from HashTagHandler import HashTagHandler
+from hashtag_handler import HashTagHandler
 
 __author__ = 'maria'
 from TweetUtils.helpers.globals import g
