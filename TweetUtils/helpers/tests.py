1601fad723e3a734f4f61135039b13c5f3effe83
Maria Karanasou <karanasou@gmail.com>
2017-08-17 18:13:13 +0300

    Update tests.py

--- TweetUtils/helpers/tests.py
+++ TweetUtils/helpers/tests.py
@@ -2,8 +2,10 @@ __author__ = 'mariakaranasou'
 
 
 def create_test_file():
+    """Create a test file with a random tweet"
     with open('../data/tweets.txt', 'w') as tf:
         for i in range(0, 8000):
             tf.write("I liked a @YouTube video http://t.co/9D1KqSWkED Classic Game Room - GRAND THEFT AUTO 3 review for PS2\n")
 
-create_test_file()
+if __name__ == "__main__":
+    create_test_file()
